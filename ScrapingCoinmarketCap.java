763a90e4e8a5daa24fadf0ff9d19487e2a94976f
jaapdew <dewijzejaap@gmail.com>
2018-09-12 15:20:40 +0200

    grote String(builder) data

--- ScrapingCoinmarketCap.java
+++ ScrapingCoinmarketCap.java
@@ -12,23 +12,34 @@ public class ScrapingCoinmarketCap {
 	
 	public static void main(String[] args) {
 		try {																			
-			Document doc = Jsoup.connect("https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20130428&end=20180911").userAgent("Mozilla/17.0").get();
-			Elements temp = doc.select("tr.text-right");
-			System.out.println(temp.size());
+			Document doc = Jsoup.connect("https://bitcoincharts.com/charts/chart.json?m=bitstampUSD&SubmitButton=Draw&r=&i=&c=0&s=&e=&Prev=&Next=&t=S&b=&a1=&m1=10&a2=&m2=25&x=0&i1=&i2=&i3=&i4=&v=1&cv=0&ps=0&l=0&p=0&")
+					.userAgent("Mozilla/17.0").get();
+			Elements temp = doc.select("body");
+			String data = temp.toString();
 			
-			int i = 0;
-				System.out.println(i + "  High  " + "   Low   ");
-			for(Element bitcoinData:temp) {
-				//bitcoinHigh.add(Double.parseDouble(bitcoinData.getElementsByIndexEquals(2).text()));
-				//bitcoinLow.add(Double.parseDouble(bitcoinData.getElementsByIndexEquals(3).text()));
-				i++;
-				System.out.println(i+ " " + bitcoinData.getElementsByIndexEquals(2).text()+ " " + bitcoinData.getElementsByIndexEquals(3).text());
-			}
+			//System.out.println(data);
+			//System.out.println(data.substring(2, 13));
+			System.out.println(data.substring(13));
+			
 		}catch(IOException e) {
 			e.printStackTrace();
 
 		}
 	}
 	
-	//<td data-format-fiat="" data-format-value="6534.25">6,534.25</td>
 }
+
+/*
+ 
+			System.out.println(temp.size());
+				int i = 0;
+				System.out.println(i + "  High  " + "   Low   ");
+				i =2;
+			for(Element bitcoinData:temp) {
+				//bitcoinHigh.add(Double.parseDouble(bitcoinData.getElementsByIndexEquals(2).text()));
+				//bitcoinLow.add(Double.parseDouble(bitcoinData.getElementsByIndexEquals(3).text()));
+				;
+			//	System.out.println(i+ " " + bitcoinData.getElementsByIndexEquals(i).text()+ " " + bitcoinData.getElementsByIndexEquals(3).text());
+				System.out.println(i+ " " + bitcoinData.getElementsByIndexEquals(i).text());
+				i= i+8;
+				*/
